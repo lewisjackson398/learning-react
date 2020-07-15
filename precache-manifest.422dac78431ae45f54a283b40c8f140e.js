3e55237543d4b40b067d8aeb1abb7b326823e59b
lewis398 <45105851+lewisjackson398@users.noreply.github.com>
2020-07-15 20:32:32 +0100

    Updates

---
+++ precache-manifest.422dac78431ae45f54a283b40c8f140e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8841e6baecd949d7fa158fdf10aaf6c9",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "ec3a9be6da8f606f0a3c",
+    "url": "/react/static/css/main.74295e73.chunk.css"
+  },
+  {
+    "revision": "89eb0ae38c30f2652c9b",
+    "url": "/react/static/js/2.abb2a86d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.abb2a86d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec3a9be6da8f606f0a3c",
+    "url": "/react/static/js/main.145fc2c4.chunk.js"
+  },
+  {
+    "revision": "a66f153a8e2251824a84",
+    "url": "/react/static/js/runtime-main.305803cd.js"
+  }
+]);
